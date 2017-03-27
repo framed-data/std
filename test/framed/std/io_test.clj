38b4a679a5c6b2eb0530544980089c1914df32c2
Andrew Berls <aberls@squareup.com>
2017-03-27 16:19:05 -0700

    Add std.io/path-join
    This joins String parts into properly-formed filepaths using the system
    separator.
    
    Ex:
    
    ```
    (std.io/path-join "foo/" "/bar" "quux.txt")
    ; => "foo/bar/quux.txt"
    ```

--- test/framed/std/io_test.clj
+++ test/framed/std/io_test.clj
@@ -1,14 +1,22 @@
 (ns framed.std.io-test
   (:require [clojure.test :refer :all]
             [clojure.java.io :as io]
-            [framed.std.io :as sio]))
+            [framed.std.io :as std.io]))
+
+(deftest test-path-get
+  (is (= "foo/bar/quux.txt" (str (std.io/path-get "foo" "bar" "quux.txt")))))
+
+(deftest test-path-join
+  (let [expected "foo/bar/quux.txt"]
+    (is (= expected (std.io/path-join "foo" "bar" "quux.txt")))
+    (is (= expected (std.io/path-join "foo/" "/bar" "quux.txt")))))
 
 (deftest test-stream-copy
   (let [contents "hello"
-        f1 (sio/spit (sio/tempfile) contents)
-        f2 (sio/tempfile)]
+        f1 (std.io/spit (std.io/tempfile) contents)
+        f2 (std.io/tempfile)]
     (is (empty? (slurp f2)))
     (with-open [istream (io/input-stream f1)
                 ostream (io/output-stream f2)]
-      (sio/stream-copy istream ostream))
+      (std.io/stream-copy istream ostream))
     (is (= contents (slurp f2)))))
